9556fb455067c23a442829f6e311419cfd413fd5
Sumesh Mohanty <80830020+SumeMonty@users.noreply.github.com>
2021-04-10 14:44:11 +0530

    Update basic.c

--- Cyber-Calculator/basic.c
+++ Cyber-Calculator/basic.c
@@ -11,48 +11,52 @@ void main()
 
         while(1)
         {
-                printf("Enter The Number Of The Option You Want To Choose\n\n");
-                printf("1.Addition Of Two Integers\n");
-                printf("2.Subtraction Between Two Integers\n");
-                printf("3.Multiplication Between Two Integers\n");
-                printf("4.Division Of Two Integers\n\n");
-                printf("5.Exit ");
-                scanf("%d", &i);
-                system("CLS");
+            printf("Enter The Number Of The Option You Want To Choose[Enter 0 To Exit]\n\n");
+            printf("1.Addition Of Two Integers\n");
+            printf("2.Subtraction Between Two Integers\n");
+            printf("3.Multiplication Between Two Integers\n");
+            printf("4.Division Of Two Integers ");
+            scanf("%d", &i);
+            system("CLS");
 
 
-                switch(i)
-                {
+            if(i==0)
+            {
+                exit(0);
+            }
 
-                        case 1:
-                                printf("Enter two numbers: ");
-                                scanf("%d%d", &a, &b);
-                                printf("\nSum Of %d And %d = %d\n", a, b, (a+b));
-                                break;
-                        case 2:
-                                printf("Enter two numbers: ");
-                                scanf("%d%d", &a, &b);
-                                printf("\nDifference Between %d And %d = %d\n", a, b, (a-b));
-                                break;
-                        case 3:
-                                printf("Enter two numbers: ");
-                                scanf("%d%d", &a, &b);
-                                printf("\nProduct Of %d And %d = %d\n", a, b, (a*b));
-                                break;
-                        case 4:
-                                printf("Enter two numbers: (Dividend   Divisor)");
-                                scanf("%d%d", &a, &b);
-                                printf("\nQuotient=%f\n", (float) a/b);
-                                printf("\nRemainder=%d\n", (a%b));
-                                break;
+            switch(i)
+            {
 
-                        case 5:
-                                exit(0);        
+                case 1:
+                        printf("Enter two numbers: ");
+                        scanf("%d%d", &a, &b);
+                        printf("\nSum Of %d And %d = %d\n", a, b, (a+b));
+                        break;
+                case 2:
+                        printf("Enter two numbers: ");
+                        scanf("%d%d", &a, &b);
+                        printf("\nDifference Between %d And %d = %d\n", a, b, (a-b));
+                        break;
+                case 3:
+                        printf("Enter two numbers: ");
+                        scanf("%d%d", &a, &b);
+                        printf("\nProduct Of %d And %d = %d\n", a, b, (a*b));
+                        break;
+                case 4:
+                        printf("Enter two numbers: (Dividend   Divisor)");
+                        scanf("%d%d", &a, &b);
+                        printf("\nQuotient=%f\n", (float) a/b);
+                        printf("\nRemainder=%d\n", (a%b));
+                        break;        
 
-                        default:
-                                MessageBox(0,"Please Choose A Valid Option","Error",1);
-                                system("basic");
-                }
+                default:
+                        MessageBox(0,"Please Choose A Valid Option","Error",1);
+                        system("basic");
+            }
+            
+            printf("\n\n\n\n");
+            
         }
 
  
